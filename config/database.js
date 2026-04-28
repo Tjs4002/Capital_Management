9fc94352cf9dc7c6b2e9aaf4bdd2491f447b8443
Tjs4002 <tejaschavan4002@gmail.com>
2026-04-28 16:09:42 +0530

    installed better-sqlite3 1

--- config/database.js
+++ config/database.js
@@ -3,8 +3,9 @@ const path = require('path');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
+  dialectModule: require('better-sqlite3'),
   storage: path.join(__dirname, 'capital_management.db'),
-  logging: false, // Set to console.log for debugging
+  logging: false,
 });
 
 module.exports = sequelize;
